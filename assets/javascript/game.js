64c594d61f0a5624cbe553ca01615b844d56d4fe
Trenton Cornwall <trenton434@gmail.com>
2019-07-04 13:57:03 -0600

    added toon images, created some objects for toons and user choice, user enemies, user defender

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+// Object of toons
+var luke = {
+    name: "luke",
+    hp: 130,
+    image: "../images/luke.png",
+
+};
+
+var yoda = {
+    name: "yoda",
+    hp: 100,
+    image: "../images/yoda.png",
+};
+
+var vader = {
+    name: "vader",
+    hp: 140,
+    image: "../images/vader.png",
+};
+
+var maul = {
+    name: "maul",
+    hap: 125,
+    image: "",
+};
+
+//use ojbect
+var userToon = {
+    toonSelected: false,
+    selected: ""
+
+};
+
+var userDefender = {
+    toonSelected: false,
+    selected: ""
+};
+
+var userEnemies = {
+    toonSelected: false,
+    selected: ""
+
+};
+
+
+$("document").ready(function(){
+    //creates object buttons
+
+    //detect click
+
+    //if user hasn't selected their toon yet, select their toon and move toons to enemies
+
+    //else select enemy
+});
