ce4670e59c510b67d9b8e4fa61b80c0c66a1f3d1
Trenton Cornwall <trenton434@gmail.com>
2019-07-04 19:26:16 -0600

    selecting a toon now will remove 1 from selector and add it to your choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@ var luke = {
     name: "luke",
     hp: 130,
     image: "assets/images/luke.png",
-
 };
 
 var yoda = {
@@ -28,13 +27,17 @@ var maul = {
 //use ojbect
 var userToon = {
     toonSelected: false,
-    selected: ""
-    
-};
+    selected: "",
 
-var userDefender = {
-    toonSelected: false,
-    selected: ""
+    selectedMe: function(x){
+        if (this.toonSelected === false) {
+            this.toonSelected = true;
+            this.selected = x;
+            console.log("u just selecet " + x)
+        };
+    },
+
+
 };
 
 var userEnemies = {
@@ -43,21 +46,32 @@ var userEnemies = {
     
 };
 
+var userDefender = {
+    toonSelected: false,
+    selected: ""
+
+};
+
+
 
 
 //creates board
 var board = {
 
     toons: [luke, yoda, vader, maul],
+    //creates inital board setup
 
-    createSelector: function(){
+    drawCards: function(){
+        var unselected = $("<div>");
+        var userSelected = $("<div>");
+        var defenderSelected = $("<div>");
+
         for (i=0; i < this.toons.length; i++) {
             //creating DIV container
             var toonBtn = $("<div>");
             toonBtn.addClass("toon toon-button toon-button-color");
             toonBtn.attr("data-name", this.toons[i].name);
            
-            
             //name container
             var toonBtnName = $("<span>");
             toonBtnName.addClass("button-name");
@@ -79,20 +93,38 @@ var board = {
             toonBtn.append(toonBtnName);
             toonBtn.append(toonBtnImg);
             toonBtn.append(toonBtnHp);
+            
 
-            //place on the page
-            $(".selectToon").append(toonBtn);
+            // This is working and not working!
+            if(this.toons[i].name === userToon.selected){
+                userSelected.append(toonBtn);
+    
+            } else if(this.toons[i].name === userDefender.selected) {
+                // unselected.append(toonBtn);
+                defenderSelected.append(toonBtn);
+            } else {
+                unselected.append(toonBtn);
+            }
         };
+        $(".selectToon").html(unselected);
+        console.log(userSelected)
+        $(".userToon").html(userSelected);
     },
+
+
 
 };
 
 $("document").ready(function(){
     //creates object buttons
-    board.createSelector();
+    board.drawCards();
     //detect click
-
-    //if user hasn't selected their toon yet, select their toon and move toons to enemies
+    $(".toon-button").on("click", function(){
+        //if user hasn't selected their toon yet
+        var toon = ($(this).attr("data-name"));
+        userToon.selectedMe(toon);
+        board.drawCards();
+    })
 
     //else select enemy
 });
