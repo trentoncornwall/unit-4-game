a9a347f5697820571763bda84c15ce299c6a3d5e
Trenton Cornwall <trenton434@gmail.com>
2019-07-06 09:48:28 -0600

    toons move from selected to userToon just fine, need to make them enemies

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,32 +25,41 @@ var maul = {
 
 
 //use ojbect
+var userDefender = {
+    toonSelected: false,
+    selected: "",
+
+    selectedMe: function (x) {
+        if (this.toonSelected === false) {
+            this.toonSelected = true;
+            this.selected = x;
+        };
+    },
+};
 var userToon = {
     toonSelected: false,
     selected: "",
 
-    selectedMe: function(x){
+    selectedMe: function (x) {
         if (this.toonSelected === false) {
             this.toonSelected = true;
             this.selected = x;
-            console.log("u just selecet " + x)
+        } else {
+            userDefender.selectedMe(x);
         };
     },
 
 
 };
 
+
+
 var userEnemies = {
     toonSelected: false,
     selected: ""
-    
-};
-
-var userDefender = {
-    toonSelected: false,
-    selected: ""
 
 };
+
 
 
 
@@ -61,23 +70,25 @@ var board = {
     toons: [luke, yoda, vader, maul],
     //creates inital board setup
 
-    drawCards: function(){
+    drawCards: function () {
+        //creates inital veriables to print to page
         var unselected = $("<div>");
         var userSelected = $("<div>");
         var defenderSelected = $("<div>");
 
-        for (i=0; i < this.toons.length; i++) {
+        //loop set to create new buttons and place them were they should go
+        for (i = 0; i < this.toons.length; i++) {
             //creating DIV container
             var toonBtn = $("<div>");
             toonBtn.addClass("toon toon-button toon-button-color");
             toonBtn.attr("data-name", this.toons[i].name);
-           
+
             //name container
             var toonBtnName = $("<span>");
             toonBtnName.addClass("button-name");
             toonBtnName.attr("id", this.toons[i].name);
             toonBtnName.text(this.toons[i].name)
-            
+
             //image container
             var toonBtnImg = $('<img>');
             toonBtnImg.addClass("button-image");
@@ -86,44 +97,47 @@ var board = {
             //health container
             var toonBtnHp = $("<span>");
             toonBtnHp.addClass("button-hp");
-            toonBtnHp.attr("data-value",this.toons[i].hp);
+            toonBtnHp.attr("data-value", this.toons[i].hp);
             toonBtnHp.text(this.toons[i].hp);
 
-            //putting it tall together
+            //putting it tall together to toonBtn
             toonBtn.append(toonBtnName);
             toonBtn.append(toonBtnImg);
             toonBtn.append(toonBtnHp);
-            
 
-            // This is working and not working!
-            if(this.toons[i].name === userToon.selected){
+
+            //identifies were toonBtn supposed to go
+            if (this.toons[i].name === userToon.selected) {
                 userSelected.append(toonBtn);
-    
-            } else if(this.toons[i].name === userDefender.selected) {
-                // unselected.append(toonBtn);
+
+            } else if (this.toons[i].name === userDefender.selected) {
+
                 defenderSelected.append(toonBtn);
             } else {
                 unselected.append(toonBtn);
             }
         };
         $(".selectToon").html(unselected);
-        console.log(userSelected)
         $(".userToon").html(userSelected);
+        $(".defendingToon").html(defenderSelected);
     },
 
 
 
 };
 
-$("document").ready(function(){
+$("document").ready(function () {
     //creates object buttons
     board.drawCards();
     //detect click
-    $(".toon-button").on("click", function(){
+    $(".toon-button").on("click", function () {
         //if user hasn't selected their toon yet
         var toon = ($(this).attr("data-name"));
+
         userToon.selectedMe(toon);
         board.drawCards();
+
+
     })
 
     //else select enemy
